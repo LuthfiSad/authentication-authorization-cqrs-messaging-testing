165a6fe3ccf08063704f66be45b71ab765c7ccbb
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-11-15 10:57:13 +0700

    feat: types for typescript

---
+++ types/video.ts
@@ -0,0 +1,12 @@
+export interface Video {
+  id: string;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  videoUrl: string;
+  category: string;
+  uploader: string;
+  views: number;
+  likes: number;
+  uploadDate: Date;
+}
